37bfdef14720f106fecb1991a905086e7dc9cf7e
GianmarcoBramucci <giangian797@gmail.com>
2024-05-06 01:26:18 +0200

    add sna5

--- sna5/seam.php
+++ sna5/seam.php
@@ -1,13 +1,23 @@
-<?php ?>
-<?php $name='BROO'?>
+
+<?php 
+    $Paragrafo='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, optio ea soluta aliquid quaerat nulla ut explicabo harum? Et dolores impedit quae quaerat praesentium iure nostrum sapiente. Odio, dolores sint.';
+    $ArrayFrasi= array_filter(explode('.',$Paragrafo));
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RAIN</title>
+    <title>HO-FINITO-LE-IDEE-PER-I-NOMI</title>
 </head>
 <body>
-    <h1><?php echo $name?></h1>
+    <h1>lorem</h1>
+    <ul>
+        <?php
+            foreach($ArrayFrasi as $Frasi){
+                echo "<li>$Frasi</li>";
+            } 
+        ?>
+    </ul>
 </body>
 </html>
