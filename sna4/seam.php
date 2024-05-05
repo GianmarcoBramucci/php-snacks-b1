c7078002f13b663dbf81e35f34efeea2e3de683d
GianmarcoBramucci <giangian797@gmail.com>
2024-05-06 01:03:23 +0200

    add sna4

--- sna4/seam.php
+++ sna4/seam.php
@@ -1,13 +1,24 @@
-<?php ?>
-<?php $name='BROO'?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RAIN</title>
+    <title>NUMERI-RANDOM</title>
 </head>
 <body>
-    <h1><?php echo $name?></h1>
+    <h1>NUMERI RANDOM</h1>
+    <ol>
+    <?php
+    $RandomArray=[];
+    while(count($RandomArray) < 15) {
+        $NumRandom= rand(0,20);
+        if(array_search($NumRandom, $RandomArray) === false) {
+            $RandomArray[]=$NumRandom;
+            echo"<li>||$NumRandom</li>";
+        }
+    }
+?>        
+    </ol>
 </body>
 </html>
